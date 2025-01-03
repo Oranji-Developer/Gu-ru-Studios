bb385d64a8bf1f6d22a2ec23492a263b6ff63056
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2025-01-03 13:12:37 +0700

    fix: import from Components to components

--- resources/js/Pages/Auth/VerifyEmail.tsx
+++ resources/js/Pages/Auth/VerifyEmail.tsx
@@ -1,7 +1,6 @@
-import PrimaryButton from "@/Components/PrimaryButton";
 import GuestLayout from "@/Layouts/GuestLayout";
 import { Button } from "@/components/ui/button";
-import { Head, Link, useForm } from "@inertiajs/react";
+import { Head, Link, useForm, usePage } from "@inertiajs/react";
 import { FormEventHandler } from "react";
 
 export default function VerifyEmail({ status }: { status?: string }) {
@@ -12,6 +11,7 @@ export default function VerifyEmail({ status }: { status?: string }) {
 
         post(route("verification.send"));
     };
+    const page = usePage();
 
     return (
         <GuestLayout>
@@ -19,12 +19,31 @@ export default function VerifyEmail({ status }: { status?: string }) {
 
             <section className="px-8 py-4 w-[calc(40vw-6rem)]">
                 <h1 className="text-[3.25rem] font-medium">
-                    Cek pesan di email kamu yuk!!
+                    Terima kasih telah mendaftar! 🎉
                 </h1>
                 <p>
-                    klik link untuk menyelesaikan pembuatan akun anda,
-                    Terimakasih!!
+                    Kami baru saja mengirimkan email verifikasi ke:
+                    {page.props.auth.user.email}
                 </p>
+
+                <p>
+                    Silakan periksa kotak masuk (atau folder spam) dan klik
+                    tautan untuk memverifikasi alamat email Anda. Langkah ini
+                    membantu kami menjaga keamanan akun Anda.
+                </p>
+
+                <h5>💡 Tidak menerima email?</h5>
+                <ul>
+                    <li>
+                        <p>Tunggu beberapa menit, mungkin perlu waktu.</p>
+                    </li>
+                    <li>
+                        <p>Periksa folder spam/sampah Anda.</p>
+                    </li>
+                    <li>
+                        <p>Masih belum berhasil?.</p>
+                    </li>
+                </ul>
 
                 {status === "verification-link-sent" && (
                     <div className="mb-4 text-sm font-medium text-green-600">
