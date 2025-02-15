96c7cf2e34be1e3d6c8e67f7276a4316d1918f27
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2025-02-15 18:57:09 +0700

    fix: classname using cn in responsive nav link component

--- resources/js/components/ResponsiveNavLink.tsx
+++ resources/js/components/ResponsiveNavLink.tsx
@@ -1,19 +1,23 @@
-import { InertiaLinkProps, Link } from '@inertiajs/react';
+import { InertiaLinkProps, Link } from "@inertiajs/react";
+import { cn } from "@/lib/utils";
 
 export default function ResponsiveNavLink({
     active = false,
-    className = '',
+    className = "",
     children,
     ...props
 }: InertiaLinkProps & { active?: boolean }) {
     return (
         <Link
             {...props}
-            className={`flex w-full items-start border-l-4 py-2 pe-4 ps-3 ${
+            className={cn(
+                "flex w-full items-center border-b py-2 pe-4 ps-3",
                 active
-                    ? 'border-indigo-400 bg-indigo-50 text-indigo-700 focus:border-indigo-700 focus:bg-indigo-100 focus:text-indigo-800'
-                    : 'border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 focus:border-gray-300 focus:bg-gray-50 focus:text-gray-800'
-            } text-base font-medium transition duration-150 ease-in-out focus:outline-none ${className}`}
+                    ? "border-purple-400 bg-purple-50 text-primary focus:border-purple-700 focus:bg-purple-100 focus:text-purple-800"
+                    : "border-transparent text-purple-300 hover:border-purple-300 hover:bg-purple-50 hover:text-primary focus:border-gray-300 focus:bg-gray-50 focus:text-gray-800",
+                "text-base font-medium transition duration-150 ease-in-out focus:outline-none",
+                className
+            )}
         >
             {children}
         </Link>
